2c60bbb4ffc81c14d5c376b5f017af0a2ad11647
Francisco Jimenez <65633635+Pamsho09@users.noreply.github.com>
2021-05-05 11:34:27 -0500

    Update App.tsx

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -16,9 +16,9 @@ import Cart from "../containers/Cart";
 import Payment from "../containers/Payment";
 import Pay from "../containers/Pay"
 const Loading = styled.div`
-  width: 1080px;
-margin:auto;
-  height: 1920px;
+  width: 100%;
+
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
